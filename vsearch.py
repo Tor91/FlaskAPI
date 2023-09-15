91809c4d0bb1ec2f97995527308f9400816dedc5
Tor91 <38109056+Tor91@users.noreply.github.com>
2023-09-15 22:02:05 +0400

    add Project

---
+++ vsearch.py
@@ -0,0 +1,10 @@
+def search4vowels(phrase: str) -> set:
+    """Return any vovels found in a suplide phrase """
+    vowels = set('aeiou')
+    return vowels.intersection(set(phrase))
+
+def search4letters(phrase: str, letters: str='aeioucd') -> set:
+    """Return set of the 'letters' foun in phrase """
+    vowels = set('aeiou')
+    return set(letters).intersection(set(phrase))
+
